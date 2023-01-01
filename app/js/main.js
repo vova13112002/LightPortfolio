cdd78b7fcf7e1b97587582dbf0ffaa7cbe9899da
Handziuk Volodymyr <vova13112002vova@gmail.com>
2023-01-01 12:04:36 +0200

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -38,12 +38,20 @@ for (let anchor of anchors) {
 //burger
 const burger = document?.querySelector('[data-burger]');
 const nav = document?.querySelector('[data-nav]');
-
+const navItems = nav?.querySelectorAll('a');
+const body = document.body;
 burger?.addEventListener('click', () => {
+  body.classList.toggle('stop-scroll');
   burger?.classList.toggle('burger--active');
   nav?.classList.toggle('nav--visible');
 });
-
+navItems.forEach(el=>{
+  el.addEventListener('click',()=>{
+    body.classList.remove('stop-scroll');
+    burger?.classList.remove('burger--active');
+    nav?.classList.remove('nav--visible');
+  });
+});
 // document.querySelectorAll('a[href*="#"]').forEach(link => {
 //   link.addEventListener('click', function (e) {
 //     e.preventDefault()
